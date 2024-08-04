904123433dd8d70d714142c7cbd2aa885d9c3b8a
Rafael S.R <110256307+rsley@users.noreply.github.com>
2024-08-04 19:05:39 +0000

    v1.0.1-Alpha | Attempt at fixing base, removed counter.

--- base.lua
+++ base.lua
@@ -2,10 +2,7 @@ getgenv().AC_VERSION = "1.0.1-Alpha"
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AscendScripting/Mercury/main/script.lua"))()
 
 print("Ascending from hell... | v" .. getgenv().AC_VERSION)
-local counter = 0
 
 repeat
   wait()
-  counter = counter + 1
 until game:IsLoaded() and game.Players.LocalPlayer
-print("It took " .. counter .. " for the game to load and the LocalPlayer to be found.")
